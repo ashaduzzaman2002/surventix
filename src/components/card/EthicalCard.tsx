eada8695d11ff55e514cef5a78188c29da5d1f7a
Ashadu Zaman <168162401+ashadu-workmates@users.noreply.github.com>
2025-04-02 16:07:54 +0530

    fix issue

--- src/components/card/EthicalCard.tsx
+++ src/components/card/EthicalCard.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import { useEffect, useRef } from "react";
-import lottie, { AnimationItem } from "lottie-web";
+import type { AnimationItem } from "lottie-web";
 
 const EthicalCard = ({
     className,
     title,
     description,
-     animationFile,
+    animationFile,
   }:
   {
     className: string;
@@ -17,34 +17,37 @@ const EthicalCard = ({
   }) => {
     const animationContainer = useRef<HTMLDivElement>(null);
     const animationInstance = useRef<AnimationItem | null>(null);
-  
+
     useEffect(() => {
       if (typeof window !== 'undefined' && animationContainer.current) {
-        animationInstance.current = lottie.loadAnimation({
-          container: animationContainer.current,
-          renderer: "svg",
-          loop: false,
-          autoplay: false,
-          animationData: animationFile,
+        // Dynamically import lottie-web
+        import("lottie-web").then((lottie) => {
+          animationInstance.current = lottie.default.loadAnimation({
+            container: animationContainer.current!,
+            renderer: "svg",
+            loop: false,
+            autoplay: false,
+            animationData: animationFile,
+          });
+
+          // Pause the animation initially
+          animationInstance.current.stop();
         });
-  
-        // Pause the animation initially
-        animationInstance.current.stop();
       }
-  
+
       return () => {
         animationInstance.current?.destroy();
       };
     }, [animationFile]);
-  
+
     const handleMouseEnter = () => {
       animationInstance.current?.goToAndPlay(0, true); // Play from start
     };
-  
+
     const handleMouseLeave = () => {
       animationInstance.current?.stop(); // Stop instead of resetting to first frame
     };
-  
+
     return (
       <div
         className={`h-[300px] w-[450px] group overflow-hidden relative ${className}`}
@@ -56,12 +59,12 @@ const EthicalCard = ({
           ref={animationContainer}
           className="absolute inset-0 pointer-events-none"
         ></div>
-  
+
         <div className="absolute inset-0">
           <div className="h-full translate-y-0 group-hover:-translate-y-[350px] duration-1000 transition-all ease-in-out flex items-end p-6 pb-8">
             <h2 className="text-3xl">{title}</h2>
           </div>
-  
+
           <div className="translate-y-[200px] group-hover:-translate-y-[300px] duration-1000 transition-all ease-in-out h-full p-6 pt-8">
             <p className="text-lg">{description}</p>
           </div>
@@ -69,5 +72,5 @@ const EthicalCard = ({
       </div>
     );
   };
-  
-  export default EthicalCard
+
+export default EthicalCard;
